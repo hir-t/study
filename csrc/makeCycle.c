f2a35ae43a61d4be5613abd7c82c3dc56a15f4c8
Hiroki <hrsca0528@hrsc.local>
2018-12-02 00:22:16 +0900

    開始地点を乱数値にした。ただし同じ数値しか出ない

--- csrc/makeCycle.c
+++ csrc/makeCycle.c
@@ -28,6 +28,7 @@ void makeCycle(){
 	}
 
 	/***	取得したものをレベル順にソート ***/
+/*
 	LINE *temp[Line_info.n_line]; //バブルソート用
 
 	for(int i = 0;i<Line_info.n_line;i++){
@@ -37,29 +38,70 @@ void makeCycle(){
 				temp[i] = Logic_level[j-1];
 				Logic_level[j-1] = Logic_level[j];
 				Logic_level[j] = temp[i];
+
 			}
 		}
-	}
-
-	/*printf("----------ソート後-----------\n");
+	}*/
+	/*
+	printf("----------ソート後-----------\n");
 	for(int i = 0;i<Line_info.n_line;i++){
-		printf("ID:%lu,入力レベル:%lu,TYPE:%u\n",Logic_level[i]->line_id,Logic_level[i]->level,Logic_level[i]->type);
-	}
-	*/
+		printf("ID:%lu,入力レベル:%lu,TYPE:%u,i:%d\n",Logic_level[i]->line_id,Logic_level[i]->level,Logic_level[i]->type,i);
+	}*/
+
 
 	/* ここからループを作る */
 	int N = 0;
+	int M = 0;
+	//LINE *next;
 	while(N < loops){
-		//for(int M = 0 M < length; M++){
-			int num = rand() % Line_info.n_line + 1; //ランダムに選択するゲート番号を選ぶ(信この値は号線なども含む)
-			while(num < 2 && 10 < num){ //ゲート以外の場合
-				num = rand() % Line_info.n_line + 1; //選択し直す
+		//int num = 10; //ループ作成の開始地点の信号線番号
+		int num = rand() % Line_info.n_line; //ランダムに選択するゲート番号を選ぶ(信この値は号線なども含む)
+		while((Logic_level[num]->type < 3 && 10 < Logic_level[num]->type)){ //ゲート以外の場合																									//(num < line->n_in - length)は修正の必要あり
+			num = rand() % Line_info.n_line; //選択し直す
+		}
+		//for(int M = 0 ;M < length; M++){
+		while(M < length){
+
+			for (int j = 0; j < line->n_out; j++){
+				line = &(Line_head[num]);
+				Logic_level[num] = line;
+				printf("selectedID:%lu,selectedTYPE:%u,outID:%lu,outTYPE:%u\n",Logic_level[num]->line_id,Logic_level[num]->type,line->out[j]->line_id,line->out[j]->type);
+				//printf("outID:%lu,outTYPE:%u,out2ID:%lu,out2TYPE:%u\n",line->out[j]->line_id,line->out[j]->type,line->out[j]->out[j]->line_id,line->out[j]->out[j]->type);
 			}
-			printf("ID1:%lu,ID2:%lu\n",Logic_level[num]->line_id,Logic_level[num+length]->line_id);
-			N++;
-		//}
+
+			num = line->out[0]->line_id; //次の信号線番号へ更新
+			if(3<line->out[0]->type && line->out[0]->type<10){ //
+				M++;
+			}
+			if(line->out[0]->type == 1){ //外部出力に到達したら
+				break;
+			}
+		}
+
+		N++;
 	}
 
 
-	
+
 }
+
+/**********************************************
+**************TYPE一覧*************************
+*
+*    TOPGUN_PI   = 0,	//!< 外部入力
+*    TOPGUN_PO   = 1,	//!< 外部出力
+*    TOPGUN_BR   = 2,	//!< ファンアウトブランチ
+*    TOPGUN_INV  = 3,	//!< インバータ
+*    TOPGUN_BUF  = 4,	//!< バッファ
+*    TOPGUN_AND  = 5,	//!< アンド
+*    TOPGUN_NAND = 6,	//!< ナンド
+*    TOPGUN_OR   = 7,	//!< オア
+*    TOPGUN_NOR  = 8,	//!< ノア
+*    TOPGUN_XOR  = 9,	//!< イクルーシブオア
+*    TOPGUN_XNOR = 10,	//!< イクルーシブノア
+*    TOPGUN_BLKI = 11,	//!< 不定入力
+*    TOPGUN_BLKO = 12,	//!< 不定出力
+*    TOPGUN_UNK  = 13,	//!< 未確定型
+*    TOPGUN_NUM_PRIM,    //!< 型の数
+*
+*/
