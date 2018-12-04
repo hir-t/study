29b4e928117417947912a480a135c823fb043915
Hiroki <hrsca0528@hrsc.local>
2018-12-04 19:17:37 +0900

    ファイル書き出し

--- csrc/eval.c
+++ csrc/eval.c
@@ -16,6 +16,19 @@ void eval(){
 	//LINE *Logic_level[Line_info.n_line];
 	LINE *line;
 
+	char out_fn[128]; //出力ファイル名を入れるchar型配列
+	char filename[128];
+	FILE *out_fp; //出力ファイル・ポインタ
+	printf("Input the name of OutputFile_name 'filename'_cnf.txt: ");
+	scanf("%s",out_fn);
+
+	sprintf(filename,"%s_cnf.txt",out_fn);
+	  out_fp = fopen(filename,"w");
+	  if(out_fp == NULL){
+	    printf("ERROR\n");
+	    exit(1);
+	  }
+
 	/*	ID順にレベルやタイプを取得 */
 	//printf("----------ソート前-----------\n");
 	for(int i = 0;i<Line_info.n_line;i++){
@@ -24,53 +37,53 @@ void eval(){
 		//input:a output:b
 		if(line->n_in == 1){
 			if(line->type==3){															// INVゲートの時
-				printf("%lu %lu 0",line->in[0]->line_id,line->out[0]->line_id);			//a + c
-				printf("-%lu -%lu 0",line->in[0]->line_id,line->out[0]->line_id);		//-a+ -c
+				fprintf(out_fp,"%lu %lu 0",line->in[0]->line_id,line->out[0]->line_id);			//a + c
+				fprintf(out_fp,"-%lu -%lu 0",line->in[0]->line_id,line->out[0]->line_id);		//-a+ -c
 			}
 			if(line->type==4){															//BUFゲートの時
-				printf("-%lu %lu 0",line->in[0]->line_id,line->out[0]->line_id);		//-a + c
-				printf("%lu -%lu 0",line->in[0]->line_id,line->out[0]->line_id);		//a+ -c
+				fprintf(out_fp,"-%lu %lu 0",line->in[0]->line_id,line->out[0]->line_id);		//-a + c
+				fprintf(out_fp,"%lu -%lu 0",line->in[0]->line_id,line->out[0]->line_id);		//a+ -c
 			}
 		}
 		//入力数が2のとき
 		//intput1:a,input2:b,output:c
 		if(line->n_in == 2){
 			if(line->type==5){																							//ANDゲートの時
-					printf("-%lu %lu 0\n", line->out[0]->line_id,line->in[0]->line_id); 								//-c + a
-					printf("-%lu %lu 0\n", line->out[0]->line_id,line->in[1]->line_id); 								//-c + b
-					printf("-%lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//-a + -b + c
+					fprintf(out_fp,"-%lu %lu 0\n", line->out[0]->line_id,line->in[0]->line_id); 								//-c + a
+					fprintf(out_fp,"-%lu %lu 0\n", line->out[0]->line_id,line->in[1]->line_id); 								//-c + b
+					fprintf(out_fp,"-%lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//-a + -b + c
 				}
 
 				if(line->type==6){																						//NANDゲートの時
-					printf("%lu %lu 0\n", line->out[0]->line_id,line->in[0]->line_id);									//c + a
-					printf("%lu %lu 0\n", line->out[0]->line_id,line->in[1]->line_id);									//c + b
-					printf("-%lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//-a + -b + -c
+					fprintf(out_fp,"%lu %lu 0\n", line->out[0]->line_id,line->in[0]->line_id);									//c + a
+					fprintf(out_fp,"%lu %lu 0\n", line->out[0]->line_id,line->in[1]->line_id);									//c + b
+					fprintf(out_fp,"-%lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//-a + -b + -c
 				}
 
 				if(line->type==7){																						//ORゲートの時
-					printf("%lu -%lu 0\n", line->out[0]->line_id,line->in[0]->line_id);									//c + -a
-					printf("%lu -%lu 0\n", line->out[0]->line_id,line->in[1]->line_id);									//c + -b
-					printf("%lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//a + b + -c
+					fprintf(out_fp,"%lu -%lu 0\n", line->out[0]->line_id,line->in[0]->line_id);									//c + -a
+					fprintf(out_fp,"%lu -%lu 0\n", line->out[0]->line_id,line->in[1]->line_id);									//c + -b
+					fprintf(out_fp,"%lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);		//a + b + -c
 				}
 
 				if(line->type==8){																						//NORゲートの時
-					printf("-%lu -%lu 0\n", line->out[0]->line_id,line->in[0]->line_id);								//-c + -a
-					printf("-%lu -%lu 0\n", line->out[0]->line_id,line->in[1]->line_id);								//-c + -b
-					printf("%lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);			//a + b + c
+					fprintf(out_fp,"-%lu -%lu 0\n", line->out[0]->line_id,line->in[0]->line_id);								//-c + -a
+					fprintf(out_fp,"-%lu -%lu 0\n", line->out[0]->line_id,line->in[1]->line_id);								//-c + -b
+					fprintf(out_fp,"%lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);			//a + b + c
 				}
 
 				if(line->type==9){																						//XORゲートの時
-					printf("%-lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//-a + b + c
-					printf("%lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + -b + c
-					printf("%-lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id); 		//-a + -b + -c
-					printf("%lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + b + -c
+					fprintf(out_fp,"%-lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//-a + b + c
+					fprintf(out_fp,"%lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + -b + c
+					fprintf(out_fp,"%-lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id); 		//-a + -b + -c
+					fprintf(out_fp,"%lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + b + -c
 				}
 
 				if(line->type==10){																						//XNORゲートの時
-					printf("%-lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//-a + b + -c
-					printf("%lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//a + -b + -c
-					printf("%-lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//-a + -b + c
-					printf("%lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + b + c
+					fprintf(out_fp,"%-lu %lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//-a + b + -c
+					fprintf(out_fp,"%lu -%lu -%lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//a + -b + -c
+					fprintf(out_fp,"%-lu -%lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   	//-a + -b + c
+					fprintf(out_fp,"%lu %lu %lu 0\n", line->in[0]->line_id,line->in[1]->line_id,line->out[0]->line_id);   		//a + b + c
 				}
 				printf("\n");
 			}
